8eb0bf554f0ab5fbcdb2a6270cdca8b0c36d0997
JackSparrow9875 <rishiveer07@outlook.com>
2023-11-03 02:21:54 +0530

    further modifications made

--- Verilog_Codes/adder_subtractor/add_sub.v
+++ Verilog_Codes/adder_subtractor/add_sub.v
@@ -12,15 +12,15 @@ module four_bit_adder_subtractor(
     wire c0,c1,c2;
 
     wire B0,B1,B2,B3;
-    xor x1(B0,B[0],m);          //when m=0, xor of B and m produces B and initial carry is also 0,
-    xor x2(B1,B[1],m);          //and this performs regular addition. When m=1, xor of B and m
-    xor x3(B2,B[2],m);          //produces B' and initial carry becomes 1, which ultimately results
-    xor x4(B3,B[3],m);          //in twos complement addition, nothing but subtraction
+    xor x1(B0,B[0],m);
+    xor x2(B1,B[1],m);
+    xor x3(B2,B[2],m);
+    xor x4(B3,B[3],m);
 
-    full_adder fa0(.a(A[0]), .b(B0), .c_in(m), .sum(Sum[0]), .carry(c0));
-    full_adder fa1(.a(A[1]), .b(B1), .c_in(c0), .sum(Sum[1]), .carry(c1));
-    full_adder fa2(.a(A[2]), .b(B2), .c_in(c1), .sum(Sum[2]), .carry(c2));
-    full_adder fa3(.a(A[3]), .b(B3), .c_in(c2), .sum(Sum[3]), .carry(Carry));
+    full_adder fa0(.a(A[0]),.b(B0),.c_in(m),.sum(Sum[0]),.carry(c0));
+    full_adder fa1(.a(A[1]),.b(B1),.c_in(c0),.sum(Sum[1]),.carry(c1));
+    full_adder fa2(.a(A[2]),.b(B2),.c_in(c1),.sum(Sum[2]),.carry(c2));
+    full_adder fa3(.a(A[3]),.b(B3),.c_in(c2),.sum(Sum[3]),.carry(Carry));
     
 endmodule
 
@@ -35,19 +35,20 @@ module full_adder(
     output sum,
     output carry);
 
-    wire c1,c2,c3;
+    wire w1,c1,c2,c3;
 
-    xor x1(sum,a,b,c_in);           //'sum' is equivalent to the Ex-OR operation performed on the three
-                                    //inputs; a,b and c_in
+    xor (w1,a,b);              //'sum' is equivalent to the Ex-OR operation performed on the three
+    xor (sum,w1,c_in);         //inputs; a,b and c_in
+    
     /*initial begin
         $display("sum = %b", sum);
     end*/
 
-    and a1(c1,a,b);
-    and a2(c2,b,c);             
-    and a3(c3,a,c);
+    and (c1,a,b);
+    and (c2,b,c_in);             
+    and (c3,a,c_in);
 
-    or o1(carry,c1,c2,c3);          //this gives us the output 'carry' that is equivalent to the sum
-                                    //of the three a and b, b and c_in and a and c_in
+    or (carry,c1,c2,c3);       //this gives us the output 'carry' that is equivalent to the sum
+                               //of the three a and b, b and c_in and a and c_in
 
 endmodule
--- Verilog_Codes/adder_subtractor/tb_add_sub.v
+++ Verilog_Codes/adder_subtractor/tb_add_sub.v
@@ -16,35 +16,36 @@ module tb_four_bit_adder_subtractor;
         .Carry(Carry));
 
     initial begin
-        $dumpfile("add_sub.vcd");
-        $dumpvars(0,UUT);
-        A = 4'b1101;        //binary equivalent of 13
-        B = 4'b0011;        //binary equivalent of 3
-        //set m to 0 for addition
-        m = 1'b0;
-        $display("A = %b, B = %b, m = %b, Sum = %b, Carry = %b", A, B, m,
-            Sum, Carry);
+    $dumpfile("add_sub.vcd");
+    $dumpvars(0,UUT);
+    A = 4'b1101;        //binary equivalent of 13
+    B = 4'b0011;        //binary equivalent of 3
+    //set m to 0 for addition
+    m = 1'b0;
+    #10;                //add a delay here
+    $display("A = %b, B = %b, m = %b", A, B, m);
+    #10;                //add a delay here
+    $display("Sum = %b, Carry = %b", Sum, Carry);
 
-        #10;
-        A = 4'b1111;        //binary equivalent of 13
-        B = 4'b0011;        //binary equivalent of 3
-        //set m to 1 for subtraction
-        m = 1'b1;
-        $display("A = %b, B = %b, m = %b, Sum = %b, Carry = %b", A, B, m,
-            Sum, Carry);
+    A = 4'b1111;        //binary equivalent of 15
+    B = 4'b0011;        //binary equivalent of 3
+    //set m to 1 for subtraction
+    m = 1'b1;
+    #10;                //add a delay here
+    $display("A = %b, B = %b, m = %b", A, B, m);
+    #10;                //add a delay here
+    $display("Sum = %b, Carry = %b", Sum, Carry);
 
-        #10;
+    A = 4'b1001;        //binary equivalent of 9
+    B = 4'b0011;        //binary equivalent of 3
+    //set m to 1 for subtraction
+    m = 1'b1;
+    #10;                //add a delay here
+    $display("A = %b, B = %b, m = %b", A, B, m);
+    #10;                //add a delay here
+    $display("Sum = %b, Carry = %b", Sum, Carry);
 
-          A = 4'b1001;        //binary equivalent of 13
-        B = 4'b0011;        //binary equivalent of 3
-        //set m to 1 for subtraction
-        m = 1'b1;
-    $display("A = %b, B = %b, m = %b, Sum = %b, Carry = %b", A, B, m,
-            Sum, Carry);
-            
-        $finish;
-    end
-
-    
+    $finish;
+end
 
 endmodule
